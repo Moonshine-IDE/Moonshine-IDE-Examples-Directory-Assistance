b927dcbf5bf258677f5e5663a8fec55e626ca27e
JoelProminic <joel@prominic.net>
2022-01-24 10:58:21 -0600

    Added placeholder Java file so that src directory could be committed.

---
+++ src/main/java/Placeholder.java
@@ -0,0 +1,10 @@
+/**
+ * A src/ directory and Java class are required in order to treat this project as a Java Maven project in Moonshine.
+ * This will be removed once we add Generic project support in Moonshine
+ */
+public class Placeholder  
+{
+	public Placeholder()
+	{
+	}
+}
